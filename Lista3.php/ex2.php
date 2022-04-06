63c01423bfd06b7265d921443d7e6f1569cea7da
joao vitor <joao.t@aluno.ifsp.edu.br>
2022-04-06 11:37:20 -0300

    Exerciucios da lista

---
+++ Lista3.php/ex2.php
@@ -0,0 +1,7 @@
+<?php
+    $vogais = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
+    $substituicao = str_replace($vogais,"x", "Sorrie mais ainda");
+
+    echo $substituicao
+
+?>
---
+++ Lista3.php/ex3.php
@@ -0,0 +1,5 @@
+<?php
+     $cpf = "50411427857";
+     $cpf =substr($cpf,8,3);
+    echo $cpf;
+?>
